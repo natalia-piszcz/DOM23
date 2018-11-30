b2a46229bee25e578a18508dae9ccd6a1fd78c22
Natalia Piszcz <np93@o2.pl>
2018-11-30 16:37:24 +0100

    book pr dom

---
+++ src/main/java/Book.java
@@ -0,0 +1,68 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private int isbn;
+
+    public Book (){
+    }
+
+    public Book(String title, String author, int year, int isbn) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", isbn=" + isbn +
+                '}';
+    }
+}
